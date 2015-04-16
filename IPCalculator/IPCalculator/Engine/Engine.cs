ed16e0f7f1b8b33d700f743107024d6eeb39ae7b
jumperabg <krasimirvelichkov@gmail.com>
2015-04-16 19:52:41 +0300

    Basic commit and assignments

---
+++ IPCalculator/IPCalculator/Engine/Engine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPCalculator.Engine
+{
+    public class Engine
+    {
+        private static Engine instance;
+
+        private Engine() { }
+
+        public static Engine Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Engine();
+                }
+                return instance;
+            }
+        }
+
+        // Sample calculator : http://www.subnet-calculator.com/subnet.php?net_class=B
+
+        // Todo Implement
+        public static string ConvertIP(string IP)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string HostAddressRange()
+        {
+            throw new NotImplementedException();
+        }
+        public static string SubnetID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string BroadcastAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string SubnetBitmap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # IPC
 IP Calculator
+
+#Tasks:
+##UI:
+-mkd1994(Marto)
+
+##Engine:
+-jumperabg(Krasi)
+-Watasama(Georgi)
+
+
+##Validator:
+-mkd1994(Marto)
+-Watasama(Georgi)
